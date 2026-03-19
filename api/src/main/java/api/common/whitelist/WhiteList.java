97fb73d01e9fa1305878a41cae9bf7d42eac3180
luvc++ <ccccssung@gmail.com>
2026-03-19 15:39:48 +0900

    commit

--- api/src/main/java/api/common/whitelist/WhiteList.java
+++ api/src/main/java/api/common/whitelist/WhiteList.java
@@ -2,7 +2,7 @@ package api.common.whitelist;
 
 public abstract class WhiteList {
     public static String[] getWhiteList = {"/connect/**","/gatherings","/v2/gatherings","/v3/gatherings","/gathering/*","/gathering","/gathering/*/meetings",
-            "/gathering/*/meeting/*","/image/*","/gathering/*/image","/gathering/*/boards","/auth/user/*","/gathering/participated/*","/health"};
+            "/gathering/*/meeting/*","/image/*","/gathering/*/image","/gathering/*/boards","/auth/user/*","/gathering/participated/*","/health","/actuator/**"};
     public static String[] postWhiteList = {"/auth/id-check","/auth/generateToken","/auth/nickname-check",
             "/auth/email-certification","/auth/check-certification","/auth/sign-in","/auth/sign-up"};
 }
