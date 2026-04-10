f56b728853dc6ec6a4ddad7c155b0903125e2a08
luvc++ <ccccssung@gmail.com>
2026-04-10 10:30:37 +0900

    v3: test 제거 후 다시 작성할 예정

--- infra/src/test/java/infra/TestApplication.java
+++
@@ -1,9 +0,0 @@
-package infra;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-@SpringBootApplication
-@EntityScan(basePackages = "entity")
-public class TestApplication {
-}
